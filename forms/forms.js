a53ac6ce53ca5cd6f9a0eeffbf3a6e7e42b2ad31
ginityson <ginityson@yahoo.com>
2016-05-01 23:47:05 -0500

    turn in sunday night

--- forms/forms.js
+++ forms/forms.js
@@ -1,24 +1,49 @@
-$(document).ready(function() {
-    var array = [];
-    $('#employeeinfo').on('submit', function(event) {
-      event.preventDefault();
+$(document).ready(function () {
+  var salaryTotal = 0.00;
+  var array = [];
 
-      var values = {};
-      $.each($('#employeeinfo').serializeArray(), function(i, field) {
-        values[field.name] = field.value;
-      })
+  $('#employeeinfo').on('submit', function (event) {
+    event.preventDefault();
+    var values = {};
+    var thisEmployeeSalary = 0.00;
 
-      console.log(values);
-
-      appendDom(values);
+    $.each($('#employeeinfo').serializeArray(), function (i, field) {
+      values[field.name] = field.value;
     });
 
-    function appendDom(empInfo) {
-      $('#container').append('<div class="person"></div>');
-      var $el = $('#container').children().last();
+    //console.log(values);
+    thisEmployeeSalary = parseFloat(values.employeeSalary);
+    salaryTotal += Math.round(thisEmployeeSalary / 12);
+    values.employeeSalary = Math.round(thisEmployeeSalary);
 
-      $el.append('<p>' + empInfo.employeefirstname + '</p>');
-    }
+    console.log(values.employeeSalary);
 
+    // clear out inputs
+    $('#employeeinfo').find('input[type=text]').val('');
 
+    // add to list
+    array.push(values);
+
+    console.log(salaryTotal);
+
+    // append to DOM
+    monthlyTotal(salaryTotal);
+    appendDom(values);
+  });
+
+  function monthlyTotal(salaryTotal) {
+    $('#salaryAmount').text('$ ' + salaryTotal);
+
+  }
+
+  function appendDom(employeeInfo) {
+    $('#corral').append('<div class="person"></div>');
+    var $el = $('#corral').children().last();
+
+    $el.append('<p>' + 'First Name: ' + employeeInfo.employeeFirstName + '</p>');
+    $el.append('<p>' + 'Last Name: ' + employeeInfo.employeeLastName + '</p>');
+    $el.append('<p>' + 'ID Number: ' + employeeInfo.employeeID + '</p>');
+    $el.append('<p>' + 'Title: ' + employeeInfo.employeeTitle + '</p>');
+    $el.append('<p>' + 'Annual Salary: ' + employeeInfo.employeeSalary + '</p>');
+  }
 });
